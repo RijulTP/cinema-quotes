7be622313f10408561a416bf3442c561b589dba1
RijulTP <rijulrajtkeey2@gmail.com>
2024-10-09 09:10:01 +0530

    Added LiveAPI widget

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,6 +1,22 @@
 // Get the current domain of the website
 const currentDomain = window.location.host;
 
+// Loading the widget on production
+if (!currentDomain.includes('localhost')) {
+  ((w, d, s) => {
+    w.liveAPI = (...args) => (w.liveAPI.q = w.liveAPI.q || []).push(args);
+    d.head.appendChild(Object.assign(d.createElement("script"), { src: s, async: true }));
+  })(window, document, "https://d2q4vn0gqet98u.cloudfront.net/latest/liveapi.js");
+
+  // Execute the widget function if liveAPI is available
+  if (typeof window.liveAPI === "function") {
+    window.liveAPI("loadwidget", {
+      selectors: ['#liveapi-code'],
+    });
+  }
+}
+
+
 // Replace the hardcoded domain in endpoint URLs with the current domain
 document.querySelectorAll("#endpoints code").forEach((code) => {
   const endpointUrl = code.textContent;
@@ -16,7 +32,8 @@ document.querySelectorAll("#endpoints code").forEach((code) => {
     updatedEndpointUrl = updatedEndpointUrl.replace(/^https:/, "http:");
   } else {
     updatedEndpointUrl = updatedEndpointUrl.replace(/^http:/, "https:");
-  }
+    updatedEndpointUrl = "GET " + updatedEndpointUrl // Adds GET , so the widget will be able to execute requests
+    }
 
   code.textContent = updatedEndpointUrl;
 });
